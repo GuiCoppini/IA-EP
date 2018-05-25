8b6e05ffe781dc0a5bfe8f69e823c0230e0f83f9
lucasbordinhoncapalbo <capalbo105>
2018-05-25 12:55:03 -0300

    Comeca a criar estrutura da arvore

--- src/ArvoreDecisao.java
+++ src/ArvoreDecisao.java
@@ -1,7 +1,11 @@
 import java.util.List;
 
 public class ArvoreDecisao {
-    //Node no = new Node();
+
+    public void criaArvore(Node raiz, List<Dado> conjunto, String melhorAtributo) {
+        raiz.nomeAtributo = melhorAtributo;
+
+    }
 
     public void criarArvore(List<Dado> conjunto) {
         //no.criarNo();
